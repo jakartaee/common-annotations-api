547589ba3864a2aa0094cb2dc48097a7b6b2b8b4
Alwin Joseph <alwin.joseph@oracle.com>
2024-02-21 21:01:10 +0530

    rename test package to ee.jakarta.tck.*

--- tck/src/main/java/ee/jakarta/tck/annotations/signaturetest/CAJSigTestIT.java
+++ tck/src/main/java/ee/jakarta/tck/annotations/signaturetest/CAJSigTestIT.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package com.sun.ts.tests.annotations;
+package ee.jakarta.tck.annotations.signaturetest;
 
 import java.util.ArrayList;
 import java.util.Properties;
